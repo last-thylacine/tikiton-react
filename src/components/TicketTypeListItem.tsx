ef029f27cf4a3d5370271042a23ab0345b446d7b
wasoract <wasoract@skiff.com>
2024-06-04 21:34:34 +0400

    descr

--- src/components/TicketTypeListItem.tsx
+++ src/components/TicketTypeListItem.tsx
@@ -3,9 +3,15 @@ import css from "./TicketTypeListItem.module.css"
 export const TicketTypeListItem = ({ ticket_type }) => (
 	<div className={css.ticketTypeListItem}>
 		<div>
-			<div>{ticket_type.name}</div>
-			<div>{ticket_type.description}</div>
+			<div className={css.ticketTypeName}>
+				{ticket_type.name}
+			</div>
+			<div className={css.ticketTypeDescription}>
+				{ticket_type.description}
+			</div>
 		</div>
-		<div>{ticket_type.cost.value} {ticket_type.cost.currency}</div>
+		<div className={css.ticketTypeCost}>
+			{`${ticket_type.cost.value} ${ticket_type.cost.currency}`}
+		</div>
 	</div>
 )
