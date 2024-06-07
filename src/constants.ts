5caed067f61bcb151dbd9bea814c9beaf52f5376
wasoract <wasoract@skiff.com>
2024-06-07 11:35:39 +0400

    total

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,4 @@ import WebApp from "@twa-dev/sdk"
 export const WEB_APP_URL = "https://last-thylacine.github.io/tikiton-react"
 export const MANIFEST_URL = `${WEB_APP_URL}/tonconnect-manifest.json`
 export const SELLER_ADDRESS = '0QC77eyH9AM0SI9_OX71vQsqsdxouNrCFLnVVe71RD_DwSB8'
-export const ONE_NANOTON = '1'
 export const IN_BROWSER = WebApp.platform === 'unknown'
--- src/hooks.ts
+++ src/hooks.ts
@@ -2,13 +2,17 @@ import WebApp from '@twa-dev/sdk'
 import { useTonConnectUI } from '@tonconnect/ui-react'
 
 import { createTransaction } from './helpers'
-import { ONE_NANOTON, SELLER_ADDRESS } from './constants'
+import { SELLER_ADDRESS } from './constants'
+import { useStore } from './store'
 
 export const usePay = () => {
 	const [tonConnectUI, _setOptions] = useTonConnectUI()
-	console.log(SELLER_ADDRESS, ONE_NANOTON)
+	const cart = useStore(state => state.cart)
+	const ticket_types = useStore(state => state.events[state.event].ticket_types)
+	const total = Object.entries(cart).reduce((acc, [ticket_id, ticket_qty]) =>
+		acc + ticket_types[ticket_id].cost.value * ticket_qty, 0)
 	const pay = async () => {
-		const transaction = createTransaction(SELLER_ADDRESS, ONE_NANOTON)
+		const transaction = createTransaction(SELLER_ADDRESS, String(total))
 		try {
 			await tonConnectUI.sendTransaction(transaction)
 			WebApp.showAlert('success')
